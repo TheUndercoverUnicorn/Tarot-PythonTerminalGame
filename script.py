b2e9ce8731d7a618554a937cbb607e18b1b5dc84
UndercoverUnicorn <37416708+TheUndercoverUnicorn@users.noreply.github.com>
2024-11-29 11:17:48 -0800

    added comments

--- script.py
+++ script.py
@@ -15,9 +15,14 @@ print("     *    *   *  *   *  *   *    *  ");
 print("     *    *   *  *   *  *   *    *  ");
 print("     *    *   *  *   *   ***     *  \n");
 
+#disclaimer
 print("Note:");
 print("According to Wikipedia, Tarot was not originally created as a tool of divination,");
 print("but as a permanent trump suit for card games. Knowing that, understand that this is");
 print("for entertainment purposes only.\n")
 
-print("Think of a question. Are you ready? Y/N")
+#have user think of a question and provide input to let you know when they are ready
+print("Think of a question.")
+ready = input("Are you ready? Y/N")
+
+if ready == Y or y:
