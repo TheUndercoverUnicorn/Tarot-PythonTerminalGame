b44263ac616faabd386cdadbd9f9e763a990095e
UndercoverUnicorn <37416708+TheUndercoverUnicorn@users.noreply.github.com>
2024-11-29 11:11:53 -0800

    add a disclaimer statement and import the random module

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
+#import the random module so you can use the function .randint()
+import random
+
+#define and initialize global variables
+
+
+#functions
+
 #ASCII Name of Game: TAROT BY SAGEUS
 print("   *****    *    ****    ***   *****");
 print("     *     * *   *   *  *   *    *  ");
@@ -7,3 +15,9 @@ print("     *    *   *  *   *  *   *    *  ");
 print("     *    *   *  *   *  *   *    *  ");
 print("     *    *   *  *   *   ***     *  \n");
 
+print("Note:");
+print("According to Wikipedia, Tarot was not originally created as a tool of divination,");
+print("but as a permanent trump suit for card games. Knowing that, understand that this is");
+print("for entertainment purposes only.\n")
+
+print("Think of a question. Are you ready? Y/N")
